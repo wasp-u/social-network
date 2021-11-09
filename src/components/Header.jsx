c99016275f55e39ec9e67cc9c5e3ebad62c54791
“wasp” <osadchiy.u@gmail.com>
2021-11-09 23:21:40 +0200

    fix 0.0.1

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png" alt="" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+const Sidebar = () => {
+    return (
+        <nav className="nav">
+            <ul>
+                <li><a href="#s">Profile</a></li>
+                <li><a href="#s">Friends</a></li>
+                <li><a href="#s">Feed</a></li>
+                <li><a href="#s">Community</a></li>
+                <li><a href="#s">Settings</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Sidebar;
